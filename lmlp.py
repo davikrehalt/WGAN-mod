b73f94774078dbc543a22f817506e25ab01a31bb
EC2 Default User <ec2-user@ip-172-31-53-239.ec2.internal>
2017-02-18 19:30:21 +0000

    changed cost

--- lmlp.py
+++ lmlp.py
@@ -44,10 +44,9 @@ class Lipshitz_Layer(object):
         self.b = b
         self.params=[self.W,self.b]
         self.output = (T.dot(self.input, self.W) + self.b).max(axis=1)
-        self.pre_gradient_norms=T.sum(abs(self.W),axis=1)
-        self.gradient_norms=tmax(self.pre_gradient_norms,1.0)
-        self.max_gradient=T.max(self.pre_gradient_norms)
-        self.gradient_cost=T.sum(T.exp(self.gradient_norms-1.0)-1.0)
+        self.gradient_norms=T.sum(abs(self.W),axis=1)
+        self.max_gradient=T.max(self.gradient_norms)
+        self.gradient_cost=T.sum(T.exp(self.gradient_norms))
 
 class LMLP(object):
     def __init__(self, rng, input, info_layers,params=None,init=0):
